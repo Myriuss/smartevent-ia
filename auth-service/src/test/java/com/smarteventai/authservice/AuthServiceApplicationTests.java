7f7fa4d8453e0a17a37c219df16cb999f0d0c40d
tre184 <74012437+tre184@users.noreply.github.com>
2026-01-17 00:45:20 +0100

    Update test auth v2

--- auth-service/src/test/java/com/smarteventai/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/smarteventai/authservice/AuthServiceApplicationTests.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+  properties = {
+    "spring.datasource.url=jdbc:postgresql://localhost:5432/auth_db",
+    "spring.datasource.username=postgres",
+    "spring.datasource.password=postgres"
+  }
+)
 @ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
